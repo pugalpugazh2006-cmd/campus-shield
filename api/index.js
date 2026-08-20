42cef4e28ccfe31b0fa92fb4651b8d1c713203c3
pugalpugazh2006-cmd <pugalpugazh2006@gmail.com>
2026-08-20 06:45:06 +0530

    chore: add error reporting to Vercel wrapper

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
-const serverless = require('../dist/serverless.js');
-
-module.exports = serverless.default || serverless;
+try {
+  const serverless = require('../dist/serverless.js');
+  module.exports = serverless.default || serverless;
+} catch (error) {
+  module.exports = (req, res) => {
+    res.status(500).json({
+      message: 'Failed to load compiled NestJS app',
+      error: error.message,
+      stack: error.stack,
+    });
+  };
+}
